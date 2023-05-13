3211b46638d44bac9c7f52eba0c02b4974a87b43
darkcat-sudo <130399755+darkcat-sudo@users.noreply.github.com>
2023-05-13 10:24:46 +0600

    jhk

---
+++ javascripitspages/video listing style/javascript.js
@@ -0,0 +1,44 @@
+
+ function myLoad ()
+  { let preload = document.getElementById('loding');
+    
+      preload.style.display = 'none';
+      
+
+   }
+   function mui(){
+    let gyo =document.querySelector("#list");
+    let nj=document.querySelector(".bac");
+    let dug=document.querySelector(".burger");
+    nj.classList.toggle("backbt");
+    gyo.classList.toggle("listr");
+    dug.classList.toggle("onburger");
+  
+  }//document.querySelector('.backbt').addEventListener('click',mui);
+function nut(){
+  let fut=document.querySelector(".vuj");
+  let but=document.querySelector(".listtop");
+ 
+  fut.classList.toggle("buj");
+  but.classList.toggle("buj");
+  
+}
+  { 
+function change() {
+    let gt = document.querySelector("#list");
+    let hyy = document.querySelector(".burger");
+    let ny = document.querySelector(".bac");
+    gt.classList.toggle("listr");
+    hyy.classList.toggle("onburger");
+    ny.classList.toggle("backbt");
+    
+}
+document.querySelector('.burger').addEventListener('click',change);
+}
+
+function suB(){
+  window.open("https://youtu.be/oO5k-0QpxTk","_blank");
+  
+
+}
+// start grinding
